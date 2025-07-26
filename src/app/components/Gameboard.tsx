d0b34fbd489c16dbea13c941f4e34e1021cd40eb
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-07-26 15:48:55 +0200

    added time to message

--- src/app/components/Gameboard.tsx
+++ src/app/components/Gameboard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export default function Gameboard() {
@@ -10,27 +10,46 @@ export default function Gameboard() {
     const [startTime, setStartTime] = useState<null | number>(null)
     const [reaction, setReaction] = useState<null | number>(null);
 
-    //doesnt run on mount
+    const timeOut = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+    //only runs when game is started
     useEffect(() => {
         if (gameActive) {
-            setTimeout(() => {
+            timeOut.current = setTimeout(() => {
                 setTimerStarted(true);
                 setStartTime(performance.now());
             }, (Math.floor(Math.random() * 7) + 1) * 1000);
         }
+        return () => {
+            if (timeOut.current) {
+                clearTimeout(timeOut.current);
+                timeOut.current = null;
+            }
+        }
     }, [gameActive]);
 
-    //doesnt run on mount
+    //runs when reaction changes
     useEffect(() => {
         if (reaction !== null) {
-            console.log(`Din reaktionsförmåga är: ${Number(reaction.toFixed(0))}ms`);
+            console.log(`Your reaction time is: ${Number(reaction.toFixed(0))}ms`);
         }
     }, [reaction])
     if (!gameActive) {
         return (
-            <div className="flex size-full"
-                onClick={() => setGameActive(true)}>
-                <p className="text-white mx-auto mt-8 text-2xl">Tryck på skärmen för att starta {failedAttempt > 0 && failedAttempt.toString()}</p>
+            <div className="flex flex-col size-full"
+                onClick={() => {
+                    setReaction(null);
+                    setGameActive(true);
+                }}>
+                <p className="text-white mx-auto mt-12 text-3xl text-center">
+                    {reaction ?
+                        Number(reaction.toFixed(0)) <= 1000 ?
+                            `Your reaction time is: ${reaction.toFixed(0)} ms` : `Your reaction time is: ${(reaction/1000).toFixed(3)} s` 
+                        : "Press the screen to start"}
+                </p> 
+                <p className="text-white mx-auto mt-4 text-xl text-center">
+                    {reaction && "Press the screen to play again!"}
+                </p>
             </div>
         )
     } else if (timerStarted) {
