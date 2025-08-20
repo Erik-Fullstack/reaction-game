869cd3356ad0b17a47ea489d1319385144636ae4
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-08-20 13:42:47 +0200

    fixed type check on vars in case of null

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -3,4 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseURL || !supabaseAnonKey) {
+    throw new Error("env vars not found");
+}
 export const supabase = createClient(supabaseURL, supabaseAnonKey)
