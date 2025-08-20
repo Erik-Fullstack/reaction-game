c39f91723a40e013e7b85b5e64b57e5740dc1c2f
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-08-20 14:20:09 +0200

    added github link to repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,17 @@
 import Menu from "./components/Menu";
 import Gameboard from "./components/Gameboard";
 import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 //TODO: trycker man innan skärmen bytt färg blir det felmeddelande (Too early)
-  //lägg till supabase för att spara scores för polarna?
+  //lägg till supabase för att spara scores?
   //bäst av 5?
 export default function Home() {
   const [reaction, setReaction] = useState<null | number>(null)
   return (
     <main className="font-sans grid grid-rows-[15vh_25vh_1fr] items-center justify-items-center min-h-screen bg-[#272727]">
+      <Link className="absolute top-2 right-2 sm:top-5 sm:right-5 xl:right-30" href={"https://github.com/Erik-Fullstack/reaction-game"}><Image src={"githubIcon.svg"} alt="Github Icon" width={30} height={30} /></Link>
       <p className="text-4xl sm:text-5xl md:text-5xl">REACTION TEST</p>
       <div className="row-start-2 flex justify-center items-center bg-[#0288d1] size-full border-[#858585]">
         <Menu reaction={reaction}/>
