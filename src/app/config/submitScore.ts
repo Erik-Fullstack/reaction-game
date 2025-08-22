745a691e63392cff52c12833547236eac3d99a44
Erik-Fullstack <erik.andersson@chasacademy.se>
2025-08-22 21:22:26 +0200

    errors from deployment vercel

--- src/app/config/submitScore.ts
+++ src/app/config/submitScore.ts
@@ -2,7 +2,7 @@
 export default async function postScore(reaction: number | null, name: string) {
     if (!reaction) return; //gör om till tooltip med ingen score att skicka
 
-    const score = await fetch("/api/submitScore", {
+    await fetch("/api/submitScore", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({name, time: reaction})
